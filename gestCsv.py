768feefec8e451237c796c73fb7b9ce916425ffb
Thomas Patois <patois.thomas@gmail.com>
2025-01-06 11:36:34 +0100

    ajout fonction createInventoryJson

--- gestCsv.py
+++ gestCsv.py
@@ -1,4 +1,4 @@
-import csv, os, fnmatch, displayVers, SearchVersionFile, displayUrl, getPlugins, subprocess, takeAppInDocker, displayVersRock, displayUrlRock
+import csv, os, fnmatch, displayVers, SearchVersionFile, displayUrl, getPlugins, subprocess, takeAppInDocker, displayVersRock, displayUrlRock, json
 
 listNameOfDock = takeAppInDocker.getNameOnlyOffice()
 listNameCollabora = takeAppInDocker.getNameCollabora()
@@ -8,7 +8,7 @@ yp = getPlugins.getPlug(listDb, listUrl)
 listVersJson = displayVers.displayVersionJson()
 listVersRock = displayVersRock.displayVersionRocket()
 
-#Création et remplissage du fichier "inventory.csv"
+#Crï¿½ation et remplissage du fichier "inventory.csv"
 def createInventory():  
   header = ["Server Name", "Url", "Cms", "Plugin or not", "Version" ]
 
@@ -48,5 +48,74 @@ def createInventory():
      # urlR = "salut"
       #cmsR = displayVersRock.listRocket[i]
       #versionR = listVersRock[i]
-      #writer.writerow([servR, urlR, cmsR, "Not a plugin", versionR])  
-      
+      #writer.writerow([servR, urlR, cmsR, "Not a plugin", versionR])
+
+#crÃ©ation et remplissage du fichier inventory.json
+def createInventoryJson():
+    inventory = []
+    
+    for i in range(len(listNameOfDock)):
+        servOf = takeAppInDocker.getServOnlyOffice()[i]
+        nameOf = listNameOfDock[i]
+        versionOf = takeAppInDocker.getVersOnlyOffice()[i]
+        inventory.append({
+            "Server Name": servOf,
+            "Url": nameOf,
+            "Cms": "OnlyOffice",
+            "Plugin or not": "Not a plugin",
+            "Version": versionOf
+        })
+    
+    for i in range(len(listUrl)):
+        version = displayVers.displayVersion()[i]
+        cms = displayVers.listCms[i]
+        url = listUrl[i]
+        serv = displayUrl.displayServ()[i]
+        inventory.append({
+            "Server Name": serv,
+            "Url": url,
+            "Cms": cms,
+            "Plugin or not": "Not a plugin",
+            "Version": version
+        })
+        if cms == "NextCloud":
+            listNamePlug = getPlugins.listNamePlg(yp)
+            for y in range(len(listNamePlug)):
+                cmsP = listNamePlug[y]
+                versionP = getPlugins.listVersPlg(yp)[y]
+                inventory.append({
+                    "Server Name": serv,
+                    "Url": url,
+                    "Cms": cmsP,
+                    "Plugin or not": "Plugin",
+                    "Version": versionP
+                })
+    
+    for i in range(len(listVersJson)):
+        servJ = displayUrl.displayServJson()[i]
+        urlJ = displayUrl.displayUrlJson()[i]
+        cmsJ = displayVers.listCmsJson[i]
+        versionJ = displayVers.displayVersionJson()[i]
+        inventory.append({
+            "Server Name": servJ,
+            "Url": urlJ,
+            "Cms": cmsJ,
+            "Plugin or not": "Not a plugin",
+            "Version": versionJ
+        })
+    
+    for i in range(len(listNameCollabora)):
+        servCo = takeAppInDocker.getServCollabora()[i]
+        nameCo = listNameCollabora[i]
+        versionCo = takeAppInDocker.getVersCollabora()[i]
+        inventory.append({
+            "Server Name": servCo,
+            "Url": nameCo,
+            "Cms": "Collabora",
+            "Plugin or not": "Not a plugin",
+            "Version": versionCo
+        })
+    
+    with open('inventory.json', 'w') as json_file:
+        json.dump(inventory, json_file, indent=4)
+
